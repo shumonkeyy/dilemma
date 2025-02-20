669e13a3f8cfc7ffc4dba935b1540cf7d1ae3503
shumonkeyy <smmoepphyu@gmail.com>
2025-02-20 15:30:26 -0800

    up till array 3 has been added.

--- src/lines.js
+++ src/lines.js
@@ -1,3 +1,25 @@
 export const STORYLINE = [
-  "June 28, 1993: The mayor tells me that 5 of my men are called in. They have not worked together before, nor are they well acquainted, but these select few were handpicked by the mayor to work on a drug runner case to form a task force led by not me, but the mayor. I was shocked. Usually, as the major of the police department, I led the task forces, but it is the mayor afterall, so who was I to say anything?",
+  {
+    line: "June 28, 1993: The mayor tells me that 5 of my men are called in. They have not worked together before, nor are they well acquainted, but these select few were handpicked by the mayor to work on a drug runner case to form a task force led by not me, but the mayor. I was shocked. Usually, as the major of the police department, I led the task forces, but it is the mayor afterall, so who was I to say anything?",
+    choice: false,
+    goto: 1,
+  },
+  {
+    line: "June 29, 1993: In the morning, I hand the deputy a walkie talkie, telling him to update me about the task on hand. While they investigate the drug case, I stand on guard waiting for updates. After a few hours, the walkie talkie suddenly beeps, transmitting the deputy’s voice. “Major”, the deputy says, updating me on where they investigated and any leads that have been made. After a few minutes, he says, “But the real reason I am updating you now is because...”",
+    choice: false,
+    goto: 2,
+  },
+  {
+    line: "“We found a dead body near the University. What should we do?”",
+    choice: true,
+    ca: "(A) Tell the force to proceed with their initial task",
+    cb: "(B) Tell them to investigate the course",
+    gotoa: 3,
+    gotob: 0,
+  },
+  {
+    line: 'I instructed the force to proceed with their initial task, knowing the mayor will be unhappy if they leave it untouched. However, the walkie talkie beeps again a few minutes later. The voice of the deputy comes once again. “Sir, we just received a report that an unidentified body has been found near the first body."',
+    choice: false,
+    goto: 0,
+  },
 ];
--- src/pages/Television.jsx
+++ src/pages/Television.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "../index.css";
 import tvImage from "../assets/tv.jpg";
 import { STORYLINE } from "../lines";
@@ -8,8 +8,15 @@ const Television = () => {
   const welcomePageRef = useRef(null);
   const storyLine = useRef(null);
   const storyBoard = useRef(null);
+  const [lineNumber, setLineNumber] = useState(0);
+  const ca = useRef(null);
+  const cb = useRef(null);
+  const choice = useRef(null);
+  const nextBtn = useRef(null);
+
   //hide welcome page to start story
-  const start = () => {
+  const start = (storyline) => {
+    const line = storyline.line;
     if (welcomePageRef.current) {
       welcomePageRef.current.classList.add("hide");
     }
@@ -17,8 +24,78 @@ const Television = () => {
       storyBoard.current.classList.remove("hide");
     }
     if (storyLine.current) {
-      storyLine.current.innerHTML = STORYLINE[0];
+      storyLine.current.innerHTML = line;
     }
+    const newLineNumber = lineNumber + 1;
+    setLineNumber(newLineNumber);
+  };
+
+  //next page
+  const next = (storyline) => {
+    const line = storyline.line;
+    const storyChoice = storyline.choice;
+    const goTo = storyline.goto;
+    if (!storyChoice) {
+      if (choice.current) {
+        if (!choice.current.classList.contains("hide")) {
+          choice.current.classList.add("hide");
+        }
+      }
+      if (nextBtn.current && nextBtn.current.classList.contains("hide")) {
+        nextBtn.current.classList.remove("hide");
+      }
+      setLineNumber(goTo);
+    } else {
+      const lineA = storyline.ca;
+      const lineB = storyline.cb;
+      if (nextBtn.current && !nextBtn.current.classList.contains("hide")) {
+        nextBtn.current.classList.add("hide");
+      }
+      if (choice.current) {
+        if (choice.current.classList.contains("hide")) {
+          choice.current.classList.remove("hide");
+        }
+        if (ca.current) {
+          ca.current.innerHTML = lineA;
+        }
+        if (cb.current) {
+          cb.current.innerHTML = lineB;
+        }
+      }
+    }
+
+    if (storyLine.current) {
+      storyLine.current.innerHTML = line;
+    }
+  };
+
+  //quit
+  const quit = () => {
+    if (welcomePageRef.current) {
+      welcomePageRef.current.classList.remove("hide");
+    }
+    if (storyBoard.current) {
+      storyBoard.current.classList.add("hide");
+    }
+    setLineNumber(0);
+  };
+
+  //choice a is picked
+  const choicea = (storyline) => {
+    const goToA = storyline.gotoa;
+    setLineNumber(goToA);
+    setTimeout(() => {
+      next(STORYLINE[goToA]);
+    }, 0);
+  };
+
+  //choice b is picked
+  const choiceb = (storyline) => {
+    const goToB = storyline.gotob;
+    setLineNumber(goToB);
+    setTimeout(() => {
+      next(STORYLINE[goToB]);
+    }, 0);
   };
 
   return (
@@ -30,7 +107,7 @@ const Television = () => {
         <div className="overlap">
           <h1>Welcome back, Major!</h1>
           <p>There is a case waiting for you.</p>
-          <button onClick={start}>Start</button>
+          <button onClick={() => start(STORYLINE[lineNumber])}>Start</button>
           <button
             type="button"
             data-bs-toggle="modal"
@@ -44,8 +121,16 @@ const Television = () => {
       <div ref={storyBoard} className="overlay-television hide">
         <div className="overlap">
           <p ref={storyLine}></p>
-          <button onClick={() => console.log("next")}>Next</button>
-          <button onClick={() => console.log("quit")}>Quit</button>
+          <div ref={choice} className="hide">
+            <p ref={ca}></p>
+            <p ref={cb}></p>
+            <button onClick={() => choicea(STORYLINE[lineNumber])}>A</button>
+            <button onClick={() => choiceb(STORYLINE[lineNumber])}>B</button>
+          </div>
+          <div ref={nextBtn}>
+            <button onClick={() => next(STORYLINE[lineNumber])}>Next</button>
+          </div>
+          <button onClick={() => quit()}>Quit</button>
         </div>
       </div>
 
